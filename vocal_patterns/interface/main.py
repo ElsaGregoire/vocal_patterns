cc93ac9eb03470aedb70f9a16658d9e9665e1010
Jake <44673908+jchaselubitz@users.noreply.github.com>
2023-11-28 17:32:32 +0100

    filled out training functions and related model stubs (#13)

--- vocal_patterns/interface/main.py
+++ vocal_patterns/interface/main.py
@@ -1,31 +1,61 @@
 import pandas as pd
-from vocal_patterns.ml_logic.model import train_model
+import numpy as np
+from sklearn.model_selection import train_test_split
+from vocal_patterns.ml_logic.data import get_data
+from vocal_patterns.ml_logic.model import compile_model, init_model, fit_model
 from vocal_patterns.ml_logic.preprocessor import (
-    load_file,
-    mel_spectrogram,
-    process_audio,
+    preprocess_audio,
 )
+from vocal_patterns.ml_logic.registry import load_model, save_model, save_results
 
 
 # @mlflow_run
-def train(X: pd.DataFrame, y: pd.DataFrame):
-    X_preprocessed = "#preprocess X"
+def train(
+    learning_rate=0.0005, batch_size=256, patience=2, split_ratio: float = 0.1
+) -> float:
+    data = get_data()
+    X_train, X_test, y_train, y_test = train_test_split(data, test_size=0.3)
+    X_train_preprocessed = preprocess_audio(X_train)
 
-    train_model(X_preprocessed, y)
-    pass
+    model = load_model()
+
+    if model is None:
+        model = init_model(input_shape=X_train_preprocessed.shape[1:])
+
+    model = compile_model(model=model, learning_rate=learning_rate)
+
+    model = fit_model(
+        model,
+        X_train_preprocessed,
+        y_train,
+        batch_size,
+        patience,
+        validation_split=split_ratio,
+    )
+
+    # Evaluate the model on the test data using `evaluate`
+    loss, accuracy = model.evaluate(X_test, y_test)
+
+    params = dict(
+        context="train",
+        loss=loss,
+        row_count=len(X_train_preprocessed),
+    )
+
+    save_results(params=params, metrics=dict(accuracy=accuracy))
+    save_model(model=model)
+
+    return accuracy
 
 
-# @mlflow_run
-def evaluate(params):
-    pass
+def predict(X_pred: pd.DataFrame = None):
+    if X_pred is None:
+        raise ValueError("No data to predict on!")
 
+    model = load_model()
+    assert model is not None
 
-def predict(params):
-    pass
+    X_pred_processed = preprocess_audio(X_pred)
+    y_pred = model.predict(X_pred_processed)
 
-
-if __name__ == "__main__":
-    preprocess(something)
-    train(something)
-    evaluate(something)
-    predict(something)
+    return y_pred
--- vocal_patterns/ml_logic/model.py
+++ vocal_patterns/ml_logic/model.py
@@ -1,11 +1,51 @@
-def init_model(model_name: str):
-    """Initializes a model from a file"""
-    pass
+from tensorflow import keras
+from keras import Model, optimizers
+from keras.callbacks import EarlyStopping
+import numpy as np
 
 
-def train_model(model, data):
-    """Trains a model"""
-    pass
+def init_model(input_shape: tuple) -> Model:
+    print("✅ Model initialized")
+
+
+def compile_model(model: Model, learning_rate=0.0005) -> Model:
+    """
+    Compile the Neural Network
+    """
+    model.compile(
+        optimizer=optimizers.Adam(learning_rate=learning_rate),
+        loss="categorical_crossentropy",
+        metrics=["accuracy"],
+    )
+
+    print("✅ Model compiled")
+    return model
+
+
+def fit_model(
+    model: Model,
+    X: np.ndarray,
+    y: np.ndarray,
+    batch_size=256,
+    patience=2,
+    # validation_data=None,  # overrides validation_split
+    validation_split=0.3,
+) -> tuple[Model, dict]:
+    es = EarlyStopping(
+        monitor="val_loss", patience=patience, restore_best_weights=True, verbose=1
+    )
+
+    model = model.fit(
+        X,
+        y,
+        # validation_data=validation_data,
+        validation_split=validation_split,
+        epochs=100,
+        batch_size=batch_size,
+        callbacks=[es],
+        verbose=0,
+    )
+    return model
 
 
 def evaluate_model(model, data):
