5c9c317915ae287bb64bc587b23cd667e4564e44
Denis Pilipenko <denis.pilipenko.90@gmail.com>
2023-11-29 15:03:25 +0100

    Model changes

--- vocal_patterns/interface/main.py
+++ vocal_patterns/interface/main.py
@@ -1,51 +1,81 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 from vocal_patterns.ml_logic.data import get_data
-from vocal_patterns.ml_logic.model import compile_model, init_model, fit_model
+from vocal_patterns.ml_logic.model import compile_model, init_model, fit_model, evaluate_model
 from vocal_patterns.ml_logic.preprocessor import (
     preprocess_audio,
 )
 from vocal_patterns.ml_logic.registry import load_model, save_model, save_results
-
+from tensorflow.keras.utils import to_categorical
 
 # @mlflow_run
 def train(
-    learning_rate=0.0005, batch_size=256, patience=2, split_ratio: float = 0.1
+    learning_rate=0.001, batch_size=32, patience=2, split_ratio: float = 0.2
 ) -> float:
     data = get_data()
-    X_train, X_test, y_train, y_test = train_test_split(data, test_size=0.3)
+
+    X = data.drop(columns=["exercise", "technique", "filename"])
+    y = data[["exercise"]]
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+
     X_train_preprocessed = preprocess_audio(X_train)
 
-    model = load_model()
+    X_train_reshaped = X_train_preprocessed.reshape(
+        len(X_train_preprocessed),
+        X_train_preprocessed.shape[1],
+        X_train_preprocessed.shape[2],
+        1,
+    )
 
-    if model is None:
-        model = init_model(input_shape=X_train_preprocessed.shape[1:])
+    X_test_preprocessed = preprocess_audio(X_test)
+    X_test_reshaped = X_test_preprocessed.reshape(
+        len(X_test_preprocessed),
+        X_test_preprocessed.shape[1],
+        X_test_preprocessed.shape[2],
+        1,
+    )
+
+    num_classes = 3
+
+    label_encoder = LabelEncoder()
+    y_train_labels = label_encoder.fit_transform(np.ravel(y_train, order="c"))
+    y_train_cat = to_categorical(y_train_labels, num_classes=num_classes)
+
+    y_test_labels = label_encoder.transform(np.ravel(y_test, order="c"))
+    y_test_cat = to_categorical(y_test_labels, num_classes=num_classes)
+
+    # model = load_model()
+    # if model is None:
+    model = init_model(input_shape=X_train_reshaped.shape[1:])
 
     model = compile_model(model=model, learning_rate=learning_rate)
 
-    model = fit_model(
+    model, history = fit_model(
         model,
-        X_train_preprocessed,
-        y_train,
+        X_train_reshaped,
+        y_train_cat,
         batch_size,
         patience,
         validation_split=split_ratio,
     )
 
     # Evaluate the model on the test data using `evaluate`
-    loss, accuracy = model.evaluate(X_test, y_test)
+    loss, accuracy = model.evaluate(X_test_reshaped, y_test_cat)
 
     params = dict(
         context="train",
         loss=loss,
-        row_count=len(X_train_preprocessed),
+        row_count=len(X_train_reshaped),
     )
 
     save_results(params=params, metrics=dict(accuracy=accuracy))
     save_model(model=model)
 
-    return accuracy
+    print(accuracy)
+    return model
 
 
 def predict(X_pred: pd.DataFrame = None):
@@ -56,6 +86,13 @@ def predict(X_pred: pd.DataFrame = None):
     assert model is not None
 
     X_pred_processed = preprocess_audio(X_pred)
-    y_pred = model.predict(X_pred_processed)
+    X_pred_reshaped = X_pred_processed.reshape(
+        len(X_pred_processed),
+        X_pred_processed.shape[1],
+        X_pred_processed.shape[2],
+        1,
+    )
+
+    y_pred = model.predict(X_pred_reshaped)
 
     return y_pred
--- vocal_patterns/ml_logic/model.py
+++ vocal_patterns/ml_logic/model.py
@@ -1,16 +1,40 @@
-from tensorflow import keras
-from keras import Model, optimizers
+from keras import optimizers, layers, models
+from tensorflow.keras.models import Sequential, Model
 from keras.callbacks import EarlyStopping
+from colorama import Fore, Style
 import numpy as np
+from typing import Tuple
 
 
-def init_model(input_shape: tuple) -> Model:
+def init_model(input_shape: Tuple) -> Model:
+    """
+    Initialize the CNN model
+    """
+    model = Sequential()
+    model.add(layers.Conv2D(8, (5,5), input_shape=input_shape, strides=(2, 2), padding='same', activation="relu"))
+    model.add(layers.BatchNormalization())
+
+    model.add(layers.Conv2D(16, (3,3), strides=(2, 2), padding='same', activation="relu"))
+    model.add(layers.BatchNormalization())
+
+    model.add(layers.Conv2D(32, (3,3), strides=(2, 2), padding='same', activation="relu"))
+    model.add(layers.BatchNormalization())
+
+    model.add(layers.Conv2D(64, (3,3), strides=(2, 2), padding='same', activation="relu"))
+    model.add(layers.BatchNormalization())
+
+    model.add(layers.Flatten())
+    model.add(layers.Dense(64, activation='relu'))
+    model.add(layers.Dense(3, activation='softmax'))
+
     print("✅ Model initialized")
 
+    return model
 
-def compile_model(model: Model, learning_rate=0.0005) -> Model:
+
+def compile_model(model: Model, learning_rate=0.001) -> Model:
     """
-    Compile the Neural Network
+    Compile the CNN
     """
     model.compile(
         optimizer=optimizers.Adam(learning_rate=learning_rate),
@@ -26,28 +50,57 @@ def fit_model(
     model: Model,
     X: np.ndarray,
     y: np.ndarray,
-    batch_size=256,
+    batch_size=32,
     patience=2,
     # validation_data=None,  # overrides validation_split
-    validation_split=0.3,
-) -> tuple[Model, dict]:
+    validation_split=0.2,
+) -> Tuple[Model, dict]:
     es = EarlyStopping(
         monitor="val_loss", patience=patience, restore_best_weights=True, verbose=1
     )
 
-    model = model.fit(
+    history = model.fit(
         X,
         y,
         # validation_data=validation_data,
         validation_split=validation_split,
-        epochs=100,
+        epochs=20,
         batch_size=batch_size,
         callbacks=[es],
         verbose=0,
     )
-    return model
+
+    return model, history
 
 
-def evaluate_model(model, data):
-    """Evaluates a model"""
-    pass
+def evaluate_model(
+        model: Model,
+        X: np.ndarray,
+        y: np.ndarray,
+        batch_size=32
+    ) -> Tuple[Model, dict]:
+    """
+    Evaluate trained model performance on the dataset
+    """
+
+    print(Fore.BLUE + f"\nEvaluating model on {len(X)} rows..." + Style.RESET_ALL)
+
+    if model is None:
+        print(f"\n❌ No model to evaluate")
+        return None
+
+    metrics = model.evaluate(
+        x=X,
+        y=y,
+        batch_size=batch_size,
+        verbose=0,
+        # callbacks=None,
+        return_dict=True
+    )
+
+    loss = metrics["loss"]
+    accuracy = metrics["accuracy"]
+
+    print(f"✅ Model evaluated, accuracy: {round(accuracy, 2)}")
+
+    return metrics
